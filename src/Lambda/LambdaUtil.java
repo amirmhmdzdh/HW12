6a7cdb062d4cec7bdbeb741a96edef2e77004168
amirmhmdzdh <amir0mohammadzadeh@gmail.com>
2024-02-27 13:22:57 +0330

    Solve Q1 - Q4

--- src/Lambda/LambdaUtil.java
+++ src/Lambda/LambdaUtil.java
@@ -1,4 +1,48 @@
 package Lambda;
 
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.util.function.*;
 public class LambdaUtil {
-}
+
+    // * Q1 *
+    public static Supplier<String> helloSupplier() {
+
+        return () -> "HellO";
+    }
+
+
+    // * Q2 *
+
+    public static Predicate<String> isEmptyPredicate() {
+        return str -> str.length() == 0;
+    }
+
+
+    // * Q3 *
+
+    public static BiFunction<String, Integer, String> stringMultiplier() {
+
+        return (str, number) -> {
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            for (int i = 0; i < number; i++) {
+                stringBuilder.append(str);
+            }
+            return stringBuilder.toString();
+
+        };
+    }
+
+        // * Q4 *
+
+        public static Function<BigDecimal, String> toDollarStringFunction() {
+
+            DecimalFormat decimalFormat = new DecimalFormat("$#,##0.00");
+            return decimalFormat::format;
+            //(BigDecimal value) -> decimalFormat.format(value);
+        }
+
+
+    }
--- src/Lambda/Main.java
+++ src/Lambda/Main.java
@@ -1,4 +1,40 @@
 package Lambda;
 
+import java.math.BigDecimal;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
 public class Main {
+    public static void main(String[] args) {
+
+        //* Q1 *
+
+        Supplier<String> helloSupplier = LambdaUtil.helloSupplier();
+        System.out.println(helloSupplier.get());//Hello
+
+
+        //* Q2 *
+
+        Predicate<String> isEmptyPredicate = LambdaUtil.isEmptyPredicate();
+
+        System.out.println(isEmptyPredicate.test("java"));//false
+        System.out.println(isEmptyPredicate.test(""));//true
+
+       // * Q3 *
+
+
+        BiFunction<String , Integer , String> stringMultiplier = LambdaUtil.stringMultiplier();
+        System.out.println(stringMultiplier.apply("Hi", 3));//HiHiHi
+
+
+        // * Q4 *
+
+        Function<BigDecimal, String> toDollarStringFunction = LambdaUtil.toDollarStringFunction();
+        String tenDollarStr = toDollarStringFunction.apply(BigDecimal.TEN.setScale(2));
+        System.out.println(tenDollarStr);//$10.00
+
+
+    }
 }
